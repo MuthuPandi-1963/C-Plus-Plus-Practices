f199de0f26bce4cc00c26e1b99a8663c19d89a50
MuthuPandi-1963 <mrprank@MrPrank.>
2024-10-19 12:31:13 +0000

    First commit

---
+++ LongestSubarrayWithSumOfK.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using  namespace std;
+void LongestSubArrayWithSumOfK(vector<int> arr,int k){
+    cout<<k<<endl;
+}
+int main(){
+    vector<int> arr={1,2,3,1,1,1,1,3,3};
+    int k=6;
+    LongestSubArrayWithSumOfK(arr,k);
+}
